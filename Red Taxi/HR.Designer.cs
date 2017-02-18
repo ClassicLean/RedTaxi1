812102347e9caf185ac72b2d3a6370737de099e7
gh0stsh0t <rjperias0213@gmail.com>
2017-02-18 12:20:55 +0800

    yawa ka undangi na

--- Red Taxi/HR.Designer.cs
+++ Red Taxi/HR.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.buttonEmp = new System.Windows.Forms.Button();
             this.buttonVehicle = new System.Windows.Forms.Button();
             this.buttonLogout = new System.Windows.Forms.Button();
@@ -37,7 +38,8 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,7 +52,7 @@
             this.buttonEmp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonEmp.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonEmp.ForeColor = System.Drawing.Color.White;
-            this.buttonEmp.Location = new System.Drawing.Point(113, 75);
+            this.buttonEmp.Location = new System.Drawing.Point(159, 72);
             this.buttonEmp.Name = "buttonEmp";
             this.buttonEmp.Size = new System.Drawing.Size(75, 23);
             this.buttonEmp.TabIndex = 0;
@@ -67,7 +69,7 @@
             this.buttonVehicle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonVehicle.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonVehicle.ForeColor = System.Drawing.Color.White;
-            this.buttonVehicle.Location = new System.Drawing.Point(113, 104);
+            this.buttonVehicle.Location = new System.Drawing.Point(159, 101);
             this.buttonVehicle.Name = "buttonVehicle";
             this.buttonVehicle.Size = new System.Drawing.Size(75, 23);
             this.buttonVehicle.TabIndex = 1;
@@ -84,7 +86,7 @@
             this.buttonLogout.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonLogout.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonLogout.ForeColor = System.Drawing.Color.White;
-            this.buttonLogout.Location = new System.Drawing.Point(113, 133);
+            this.buttonLogout.Location = new System.Drawing.Point(159, 130);
             this.buttonLogout.Name = "buttonLogout";
             this.buttonLogout.Size = new System.Drawing.Size(75, 23);
             this.buttonLogout.TabIndex = 2;
@@ -95,6 +97,7 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(69)))), ((int)(((byte)(95)))));
+            this.panel2.Controls.Add(this.button2);
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(18, 264);
@@ -103,7 +106,6 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(69)))), ((int)(((byte)(95)))));
-            this.panel1.Controls.Add(this.button2);
             this.panel1.Location = new System.Drawing.Point(374, -1);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(18, 264);
@@ -121,7 +123,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Roboto", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(-10, 0);
+            this.button2.Location = new System.Drawing.Point(0, -1);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(25, 25);
             this.button2.TabIndex = 9;
@@ -130,12 +132,13 @@
             // 
             // panel3
             // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(206)))), ((int)(((byte)(69)))), ((int)(((byte)(95)))));
-            this.panel3.Controls.Add(this.button1);
             this.panel3.Controls.Add(this.button3);
-            this.panel3.Location = new System.Drawing.Point(323, -2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(0, -1);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(162, 265);
+            this.panel3.Size = new System.Drawing.Size(136, 191);
             this.panel3.TabIndex = 9;
             // 
             // button1
@@ -149,7 +152,7 @@
             this.button1.ForeColor = System.Drawing.Color.White;
             this.button1.Location = new System.Drawing.Point(0, 48);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(105, 41);
+            this.button1.Size = new System.Drawing.Size(134, 41);
             this.button1.TabIndex = 9;
             this.button1.Text = "Logout";
             this.button1.UseVisualStyleBackColor = false;
@@ -165,10 +168,15 @@
             this.button3.ForeColor = System.Drawing.Color.White;
             this.button3.Location = new System.Drawing.Point(0, 93);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(105, 40);
+            this.button3.Size = new System.Drawing.Size(134, 40);
             this.button3.TabIndex = 1;
             this.button3.Text = "Logout";
             this.button3.UseVisualStyleBackColor = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // HR
             // 
@@ -176,19 +184,19 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(231)))), ((int)(((byte)(255)))));
             this.ClientSize = new System.Drawing.Size(392, 261);
-            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.buttonLogout);
             this.Controls.Add(this.buttonVehicle);
             this.Controls.Add(this.buttonEmp);
+            this.Controls.Add(this.panel3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "HR";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HR";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.HR_FormClosing);
             this.Load += new System.EventHandler(this.HR_Load);
-            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -205,5 +213,6 @@
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- Red Taxi/HR.cs
+++ Red Taxi/HR.cs
@@ -15,9 +15,13 @@ namespace Red_Taxi
         public Form1 reference_to_form1 { get; set; }
         public Employee reference_to_employee { get; set; }
         public Vehicle reference_to_vehicle { get; set; }
+        int oldxsize = 0;
+        int oldysize = 0;
+        int increm = -1;
         public HR()
         {
             InitializeComponent();
+            panel3.Size = new Size(0, 0);
         }
 
         private void HR_Load(object sender, EventArgs e)
@@ -51,15 +55,36 @@ namespace Red_Taxi
             veh.Show();
             this.Hide();
         }
+            int yawa = 0;
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            increm = increm * -1;
+            timer1.Start();
+            yawa++;
+            if (yawa == 10)
+            {
+                MessageBox.Show("yawa ka, undangi na");
+                yawa = 0;
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
+            
+        }
 
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel3.Size = new Size(oldxsize+=8*increm, oldysize+=80*increm);
+            if (oldxsize >= 136 && oldysize >= 191 && increm > 0)
+            {
+                timer1.Stop();
+            }
+            else if (oldxsize == 0 && oldysize == 0)
+            {
+                timer1.Stop();
+            }
         }
     }
 }
